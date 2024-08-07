6615ab4152c2ea4e4159383e3f51d8ae1f8a3507
Anmol Sinha <anmol1201@gmail.com>
2024-08-07 05:34:18 +0300

    Added basic tests

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,10 +1,50 @@
+using ParserSharp;
+
 namespace Test;
 
-public class UnitTest1
+public class BasicParsing
 {
-    [Fact]
-    public void Test1()
-    {
+	[Fact]
+	public void Success_Complete()
+	{
+		var parser = new Parser()
+			.FromText("123");
 
-    }
+		var result = parser.Parse("123");
+
+		Assert.True(result.Success);
+		Assert.Equal(3, result.CursorPosition);
+		Assert.Equal("123", result.Text.ToString());
+	}
+
+	[Fact]
+	public void Success_Partial()
+	{
+		var parser = new Parser()
+			.FromText("123");
+
+		var result = parser.Parse("1234");
+
+		Assert.True(result.Success);
+		Assert.Equal(3, result.CursorPosition);
+		Assert.Equal("123", result.Text.ToString());
+	}
+
+	[Fact]
+	public void Failure_Partial()
+	{
+		var parser = new Parser()
+			.FromText("123");
+
+		Assert.Throws<Exception>(() => parser.Parse("12"));
+	}
+
+	[Fact]
+	public void Failure_SomethingElse()
+	{
+		var parser = new Parser()
+			.FromText("123");
+
+		Assert.Throws<Exception>(() => parser.Parse("abc"));
+	}
 }
