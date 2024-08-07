518bcc617ba3115d63f6be1a4129336fa3c86027
Anmol Sinha <anmol1201@gmail.com>
2024-08-07 04:57:52 +0300

    Changed return type to ReadOnlySpan

--- Source/Helpers/Span.cs
+++ Source/Helpers/Span.cs
@@ -2,10 +2,10 @@ namespace ParserSharp;
 
 public static partial class Helpers
 {
-	public static string Truncate(this ReadOnlySpan<char> inputSpan, int maxLength, string suffix = "...")
+	public static ReadOnlySpan<char> Truncate(this ReadOnlySpan<char> inputSpan, int maxLength, string suffix = "...")
 	{
 		if (inputSpan.Length <= maxLength)
-			return inputSpan.ToString();
-		return $"{inputSpan}{suffix}";
+			return inputSpan;
+		return $"{inputSpan[..maxLength]}{suffix}";
 	}
 }
