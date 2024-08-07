7f040a7c377c0fe3b92ab8347a3e3210f7e59b8a
Anmol Sinha <anmol1201@gmail.com>
2024-08-07 05:42:08 +0300

    Added basic tokenization tests

---
+++ Test/Tokenization.cs
@@ -0,0 +1,31 @@
+using ParserSharp;
+
+namespace ParserSharp.Tests;
+
+public class Tokenization
+{
+	[Fact]
+	public void Success_TokenizeAs_NotTriggered()
+	{
+		var parser = new Parser()
+			.FromText("123")
+			.TokenizeAs("Number");
+
+		var result = parser.Parse("123");
+
+		Assert.True(result.Success);
+		Assert.Equal(3, result.CursorPosition);
+		Assert.Equal("123", result.Text.ToString());
+	}
+
+	[Fact]
+	public void Failure_TokenizeAs_Triggered()
+	{
+		var parser = new Parser()
+			.FromText("123")
+			.TokenizeAs("Number");
+
+		var exception = Assert.Throws<Exception>(() => parser.Parse("abc"));
+		Assert.True(exception.Message.Contains("Number"));
+	}
+}
