46c4794e13283213b469add0b39be284e14c7e74
Anmol Sinha <anmol1201@gmail.com>
2024-08-27 16:31:47 +0300

    Added Then primitive

--- Source/Program.cs
+++ Source/Program.cs
@@ -3,9 +3,14 @@
 var foo = new Parser()
 	.FromText("123")
 	.TokenizeAs("Number");
+
+var bar = new Parser()
+	.FromText("0")
+	.Then(foo);
+
 // char[] toParse = ['1', '2', '3', '1', '2', '3'];
-var toParse = "asd";
-var result = foo.Parse(toParse);
+var toParse = "0123";
+var result = bar.Parse(toParse);
 // toParse[1] = '4';
 
 Console.WriteLine("foobar");
